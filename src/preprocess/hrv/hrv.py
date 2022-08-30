4bc34862ad161311dc121c29341ae96122989192
DimensionSTP <ddang8jh@gmail.com>
2022-08-30 21:42:46 +0900

    feat: hrv sync with video fps

--- src/preprocess/hrv/hrv.py
+++ src/preprocess/hrv/hrv.py
@@ -41,60 +41,12 @@ class HRV:
         """
         before_ppi = self.calc_ppi(peak=peak, sampling_rate=sampling_rate)
         ppi = self.ppi_correction(ppi=before_ppi)
-        # df (re-sampling: 2 Hz)
-        dt = 0.5
-        fs = 1 / dt
-        df = fs / len(ppi)
 
-        # fft
-        power_spectrum = self.calc_fft(ppi)
-
-        # time domain
-        mean_ppi = np.average(ppi)
         bpm = round(self.calc_bpm(ppi), 1)
         if (np.isnan(bpm)) | (np.isinf(bpm)):  # exception
             return [np.nan] * 21
-        # sdnn = self.calc_sdnn(ppi)
-        # rmssd = self.calc_rmssd(ppi)
-        # pnn50 = self.calc_pnn50(ppi)
-        # rsa_pb = self.calc_rsa(ppi)
 
-        # # frequency domain
-        # vlf, lf, hf = self.calc_f(power_spectrum, df)
-        # vlf_p, lf_p, hf_p = self.calc_f_p(vlf, lf, hf)
-        # ln_vlf, ln_lf, ln_hf = self.calc_ln_f(vlf, lf, hf)
-        # vlf_hf, lf_hf = self.calc_per_hf(vlf, lf, hf)
-
-        # t_pow = self.calc_t_pow(vlf, lf, hf)
-        # d_pow, d_hz = self.calc_d(power_spectrum, df)
-        # p_pow, p_hz = self.calc_p(power_spectrum, df)
-        # coh_ratio = self.calc_coh_ratio(t_pow, p_pow)
-
-        return [
-            # mean_ppi,
-            bpm
-            # sdnn,
-            # rmssd,
-            # pnn50,
-            # vlf,
-            # lf,
-            # hf,
-            # vlf_p,
-            # lf_p,
-            # hf_p,
-            # ln_vlf,
-            # ln_lf,
-            # ln_hf,
-            # vlf_hf,
-            # lf_hf,
-            # t_pow,
-            # d_pow,
-            # d_hz,
-            # p_pow,
-            # p_hz,
-            # coh_ratio,
-            # rsa_pb
-        ]
+        return [bpm]
 
     # PPI (= RRI, IBI(interbeat interval))
     def calc_ppi(self, peak, sampling_rate):
@@ -132,8 +84,6 @@ class HRV:
 
             else:
                 mean_b4 = np.nanmean(_nni[k - 4 : k - 1])
-                # min_b4 = mean_b4 - 2 * np.std(_nni[k - 4: k - 1])
-                # max_b4 = mean_b4 + 2 * np.std(_nni[k - 4: k - 1])
 
                 if (_nni[k] < mean_b4 * 0.8) or (
                     _nni[k] > mean_b4 * 1.3
@@ -143,7 +93,6 @@ class HRV:
 
                 else:
                     nni.append(_nni[k])  # 위의 조건에 해당하지 않으면 원래값 그대로
-        print(len(nni))
         return nni
 
     # Time domain
@@ -153,8 +102,6 @@ class HRV:
         :return:
         """
         ppi = np.trim_zeros(ppi)
-        # for _ in range(ppi.count(0)):
-        #     ppi.remove(0)
         avg_ppi = np.average(ppi)
         bpm = 60000.0 / avg_ppi
         return bpm
@@ -193,10 +140,6 @@ class HRV:
     def calc_rsa(self, ppi, epoch=30, interval=30):
         epoch_lnvar_list = []
         rsa_pb = 0
-        # # 2Hz resampling (2Hz resampling이 안되었다면 이걸 살려도 됨)
-        # secs = int(len(ecg) / 500)  # Number of seconds in signal X  (ppi는 샘플링 수를 정해줘야하는 신호이기 떄문에 시간을 알려면 ecg 신호로 대체해서 하는수밖에 없음)
-        # samps = secs * 2  # Number of samples to downsample
-        # resampled = resample(ppi, samps)
 
         # Savitzky Golay filter (Moving polynomial)
         savitzky = savgol_filter(ppi, 21, 3)  # 21 point filter, 3rd order
--- src/preprocess/hrv/main.py
+++ src/preprocess/hrv/main.py
@@ -2,90 +2,61 @@ import os
 from typing import List
 
 import pandas as pd
+import cv2
 
 from hrv import HRV
 import maxima_peak_detection as mpd
 
-WINDOW_SIZE = 10
 INTERVAL_SIZE = 0.5
 SAMPLING_RATE = 500
-DATA_PATH = "./data"
-FILE_NAME = os.listdir(DATA_PATH)
-SAVE_PATH = "./result"
+DATA_PATH = "./preprocessing/hrv/data"
+VIDEO_PATH = "./preprocessing/raw/cam_under300/"
+FILE_NAME = os.listdir(VIDEO_PATH)
+SAVE_PATH = "./preprocessing/hrv/result"
 hrv = HRV()
 
-
 def main(
-    window_size: int,
     interval_size: int,
-    sampling_rate: int,
-    data_path: str,
-    file_name: List,
-    save_path: str,
+    sampling_rate: int, 
+    data_path: str, 
+    video_path: str, 
+    file_name: List, 
+    save_path: str
 ):
     for file in file_name:
-        ppg_df = pd.read_csv(
-            f"{data_path}/{file}", header=None, delimiter="\t", skiprows=23
-        )
-        print(file + "분석 시작!")
-
+        cap = cv2.VideoCapture(os.path.join(video_path, file))
+        frame_rate = cap.get(5)
+        cap.release()
+        window_size = int(300 / frame_rate)
+        
+        ppg_df = pd.read_csv(f"{data_path}/{file[:-4]}.txt", header=None, delimiter="\t", skiprows=23)
+        print("-" * 200)
+        print(f"{file[:-4]}.txt + 분석 시작!")
+        
         ppg_value = ppg_df[1].values
         # Sliding window
         hrv_list = []
-
-        for i in range(
-            0,
-            len(ppg_value) - window_size * sampling_rate,
-            int(interval_size * sampling_rate),
-        ):
-            sliding_ppg = ppg_value[i : i + window_size * sampling_rate]
+        
+        for i in range(0, len(ppg_value) - window_size * sampling_rate, int(interval_size * sampling_rate)):
+            sliding_ppg = ppg_value[i:i + window_size * sampling_rate]
             sliding_ppg = sliding_ppg.astype("float")
             # Peak detection
-            peak = mpd.detect_peaks(
-                sliding_ppg, fs=sampling_rate, detrend_factor=1, is_display=False
-            )
+            peak = mpd.detect_peaks(sliding_ppg, fs=sampling_rate, detrend_factor=1, is_display=False)
             # Calcualte HRV
             hrv_list.append(hrv(peak=peak, sampling_rate=sampling_rate))
         # file write (.csv)
         df = pd.DataFrame(hrv_list)
-        df.columns = [
-            # "RRI",
-            "BPM"
-            # "SDNN",
-            # "rMSSD",
-            # "pNN50",
-            # "VLF",
-            # "LF",
-            # "HF",
-            # "VLFp",
-            # "LFp",
-            # "HFp",
-            # "lnVLF",
-            # "lnLF",
-            # "lnHF",
-            # "VLF/HF",
-            # "LF/HF",
-            # "tPow",
-            # "dPow",
-            # "dHz",
-            # "pPow",
-            # "pHz",
-            # "CohRatio",
-            # "RSA_PB"
-        ]
-        df.to_csv(
-            f"{save_path}/{file[:-4]}_{window_size}.csv",
-            encoding="utf-8-sig",
-            index=False,
-        )  # 결과 저장
-
-
+        df.columns = ["BPM"]
+        df.to_csv(f"{save_path}/{file[:-4]}_300.csv", encoding="utf-8-sig", index=False) #결과 저장
+        print(f"{file[:-4]}.csv + 분석 결과 저장 완료!")
+        print("-" * 200)
+        
 if __name__ == "__main__":
     main(
-        window_size=WINDOW_SIZE,
-        interval_size=INTERVAL_SIZE,
-        sampling_rate=SAMPLING_RATE,
-        data_path=DATA_PATH,
-        file_name=FILE_NAME,
-        save_path=SAVE_PATH,
+        interval_size=INTERVAL_SIZE, 
+        sampling_rate=SAMPLING_RATE, 
+        data_path = DATA_PATH, 
+        video_path = VIDEO_PATH, 
+        file_name=FILE_NAME, 
+        save_path = SAVE_PATH
     )
