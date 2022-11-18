48927800a28da616e04fbcea65300a4c7f05fcec
DimensionSTP <ddang8jh@gmail.com>
2022-11-18 17:59:40 +0900

    feat: dataset class for both windows and linux

--- src/dataset_modules/datasets/stmap_dataset.py
+++ src/dataset_modules/datasets/stmap_dataset.py
@@ -11,16 +11,19 @@ from torch.utils.data import Dataset
 class CustomDataset(Dataset):
     def __init__(self, data_path: str, split: str):
         super().__init__()
+        self.data_path = data_path
+        self.split = split
         self.data_list = self.load_data(data_path, split)
 
     def __len__(self):
         return len(self.data_list)
 
     def __getitem__(self, idx):
-        stmap_path = self.data_list[idx].split("\\")[0][:-5]
-        stmap_name = self.data_list[idx].split("\\")[1][:-4] + ".npy"
+        stmap_path = self.data_path[:-1]
+        stmap_name = self.data_list[idx].split("/")[-1][:-4] + ".npy"
         stmap = torch.tensor(
-            np.load(f"{stmap_path}/stmaps/{stmap_name}"), dtype=torch.float32
+            np.load(f"{stmap_path}/{self.split}/stmaps/{stmap_name}"),
+            dtype=torch.float32,
         )
         label_df = pd.read_csv(self.data_list[idx])
         label = torch.tensor(list(label_df["BPM"]), dtype=torch.float32)
@@ -29,30 +32,5 @@ class CustomDataset(Dataset):
     @staticmethod
     def load_data(data_path: str, split: str) -> List:
         data_list = glob.glob(data_path + split + "/hrvs/*.csv")
+        data_list = [i.replace("\\", "/", 10) for i in data_list]
         return data_list
-
-
-# class CustomDataset(Dataset):
-#     def __init__(self, data_path: str, split: str):
-#         super().__init__()
-#         self.data_path = data_path
-#         self.split = split
-#         self.data_list = self.load_data(data_path, split)
-
-#     def __len__(self):
-#         return len(self.data_list)
-
-#     def __getitem__(self, idx):
-#         stmap_path = self.data_path[:-1]
-#         stmap_name = self.data_list[idx].split("/")[-1][:-4] + ".npy"
-#         stmap = torch.tensor(
-#             np.load(f"{stmap_path}/{self.split}/stmaps/{stmap_name}"), dtype=torch.float32
-#         )
-#         label_df = pd.read_csv(self.data_list[idx])
-#         label = torch.tensor(list(label_df["BPM"]), dtype=torch.float32)
-#         return stmap, label
-
-#     @staticmethod
-#     def load_data(data_path: str, split: str) -> List:
-#         data_list = glob.glob(data_path + split + "/hrvs/*.csv")
-#         return data_list
