54296c268a469156de4f80fa763c0f5799119158
DimensionSTP <ddang8jh@gmail.com>
2024-02-12 06:33:46 +0900

    update: fit to lightning 2.0, delete rank_zero_info

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import json
 import hydra
 from omegaconf import OmegaConf, DictConfig
 
-from pytorch_lightning.utilities.distributed import rank_zero_info
-
 from src.pipelines.pipeline import train, test, predict, tune
 
 
 @hydra.main(config_path="configs/", config_name="rhythm.yaml")
-def main(config: DictConfig,) -> None:
+def main(
+    config: DictConfig,
+) -> None:
     if config.is_tuned:
         params = json.load(open(config.tuned_hparams_path, "rt", encoding="UTF-8"))
         config = OmegaConf.merge(config, params)
-    rank_zero_info(OmegaConf.to_yaml(config))
 
     if config.mode == "train":
         return train(config)
