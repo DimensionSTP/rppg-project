7b48c17ee99fe688da6cfde6109c07df2961fa55
DimensionSTP <ddang8jh@gmail.com>
2024-07-20 06:10:17 +0900

    feat: weight decay hparams tuning

--- src/tuners/rhythm_tuner.py
+++ src/tuners/rhythm_tuner.py
@@ -109,6 +109,13 @@ class RhythmTuner:
                 high=self.hparams.lr.high,
                 log=self.hparams.lr.log,
             )
+        if self.hparams.weight_decay:
+            params["weight_decay"] = trial.suggest_float(
+                name="weight_decay",
+                low=self.hparams.weight_decay.low,
+                high=self.hparams.weight_decay.high,
+                log=self.hparams.weight_decay.log,
+            )
         if self.hparams.period:
             params["period"] = trial.suggest_int(
                 name="period",
@@ -135,6 +142,7 @@ class RhythmTuner:
             model=model,
             strategy=self.module_params.strategy,
             lr=params["lr"],
+            weight_decay=params["weight_decay"],
             period=params["period"],
             eta_min=params["eta_min"],
             interval=self.module_params.interval,
