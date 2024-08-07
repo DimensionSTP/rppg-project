6b9d55988b810dc5bfe24a0e994647681e914dd5
DimensionSTP <ddang8jh@gmail.com>
2024-08-07 13:48:10 +0900

    feat: model_detail, merge_model

--- scripts/prepare_upload.sh
+++ scripts/prepare_upload.sh
@@ -7,6 +7,7 @@ strategy="ddp"
 precision=32
 batch_size=64
 epoch=10
+model_detail="RhythmNet-customized"
 
 python $path/prepare_upload.py \
     is_tuned=$is_tuned \
@@ -14,4 +15,5 @@ python $path/prepare_upload.py \
     strategy=$strategy \
     precision=$precision \
     batch_size=$batch_size \
-    epoch=$epoch
+    epoch=$epoch \
+    model_detail=$model_detail
