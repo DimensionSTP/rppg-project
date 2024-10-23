3b3abdfacab2552541b9025cc5572df3cbf73457
DimensionSTP <ddang8jh@gmail.com>
2024-10-23 18:59:55 +0900

    feat: variable names, method locations, refactoring all

--- src/architectures/losses/combined_label_distribution_loss.py
+++ src/architectures/losses/combined_label_distribution_loss.py
@@ -1,151 +1,14 @@
 from typing import Dict
 import math
 
-import numpy as np
-
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torch.autograd import Variable
 
 
 class CombinedLabelDistributionLoss(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-
-    @staticmethod
-    def normal_distribution(
-        bpm: torch.Tensor,
-        min_bpm: int,
-        max_bpm: int,
-        std: float,
-    ) -> Dict[str, torch.Tensor]:
-        scaled_bpm = bpm - min_bpm
-        range_size = max_bpm - min_bpm
-        bpm_range = torch.arange(0, range_size).float().unsqueeze(0)
-        diff = bpm_range - scaled_bpm
-        exponent = -(diff**2) / (2 * std**2)
-        normal_distribution = torch.exp(exponent) / (
-            torch.sqrt(2 * torch.tensor(math.pi)) * std
-        )
-        normal_distribution = torch.clamp(normal_distribution, min=1e-15)
-        return {
-            "normal_distribution": normal_distribution,
-            "bpm_range": bpm_range,
-        }
-
-    @staticmethod
-    def kl_divergence_loss(
-        pred: torch.Tensor,
-        target: torch.Tensor,
-    ) -> torch.Tensor:
-        criterion = nn.KLDivLoss(reduction="batchmean")
-        log_pred = torch.log(pred)
-        loss = criterion(log_pred, target)
-        return loss
-
-    def negative_pearson_loss(
-        self,
-        pred: torch.Tensor,
-        target: torch.Tensor,
-    ) -> torch.Tensor:
-        pred_mean = pred.mean(
-            dim=1,
-            keepdim=True,
-        )
-        target_mean = target.mean(
-            dim=1,
-            keepdim=True,
-        )
-
-        pred_centered = pred - pred_mean
-        target_centered = target - target_mean
-
-        pred_std = pred_centered.pow(2).sum(dim=1).sqrt()
-        target_std = target_centered.pow(2).sum(dim=1).sqrt()
-
-        covariance = (pred_centered * target_centered).sum(dim=1)
-
-        pearson_correlation = covariance / (pred_std * target_std)
-
-        loss = 1 - pearson_correlation
-        return loss.mean()
-
-    def complex_absolute_given_k(self, output, k, N):
-        two_pi_n_over_N = (
-            Variable(
-                2 * math.pi * torch.arange(0, N, dtype=torch.float), requires_grad=True
-            )
-            / N
-        )
-        hanning_window = Variable(
-            torch.from_numpy(np.hanning(N)).type(torch.FloatTensor), requires_grad=True
-        ).view(1, -1)
-
-        k = k.type(torch.FloatTensor).cuda()
-        two_pi_n_over_N = two_pi_n_over_N.cuda()
-        hanning_window = hanning_window.cuda()
-
-        output = output.view(1, -1) * hanning_window
-        output = output.view(1, 1, -1).type(torch.cuda.FloatTensor)
-        k = k.view(1, -1, 1)
-        two_pi_n_over_N = two_pi_n_over_N.view(1, 1, -1)
-
-        complex_absolute = (
-            torch.sum(output * torch.sin(k * two_pi_n_over_N), dim=-1) ** 2
-            + torch.sum(output * torch.cos(k * two_pi_n_over_N), dim=-1) ** 2
-        )
-
-        return complex_absolute
-
-    def complex_absolute(self, output, sampling_rate, bpm_range):
-        output = output.view(1, -1)
-        N = output.size()[1]
-        unit_per_hz = sampling_rate / N
-        feasible_bpm = bpm_range / 60.0
-        k = feasible_bpm / unit_per_hz
-        complex_absolute = self.complex_absolute_given_k(output, k, N)
-        return (1.0 / complex_absolute.sum()) * complex_absolute
-
-    def combined_loss(
-        self,
-        pred: torch.Tensor,
-        bpm: torch.Tensor,
-        min_bpm: int,
-        max_bpm: int,
-        std: float,
-        frame_rate: torch.Tensor,
-    ) -> Dict[str, torch.Tensor]:
-        distribution_outputs = self.normal_distribution(
-            bpm,
-            min_bpm,
-            max_bpm,
-            std,
-        )
-        bpm_distribution = distribution_outputs["normal_distribution"]
-        bpm_range = distribution_outputs["bpm_range"]
-
-        pred = pred.view(1, -1)
-        bpm = bpm.view(1, -1)
-
-        complex_absolute = self.complex_absolute(pred, frame_rate, bpm_range)
-        frequency_distribution = F.softmax(
-            complex_absolute.view(-1),
-            dim=0,
-        )
-        kl_div_loss = self.kl_divergence_loss(
-            pred=frequency_distribution,
-            target=bpm_distribution,
-        )
-
-        whole_max_val, whole_max_idx = complex_absolute.view(-1).max(0)
-        whole_max_idx = whole_max_idx.type(torch.float)
-
-        return (
-            kl_div_loss,
-            F.cross_entropy(complex_absolute, bpm.view((1)).type(torch.long)),
-            torch.abs(bpm[0] - whole_max_idx),
-        )
 
     def forward(
         self,
@@ -191,6 +54,194 @@ class CombinedLabelDistributionLoss(nn.Module):
         return {
             "total_loss": total_loss,
             "rppg_loss": rppg_loss,
+            "kl_div_loss": kl_div_loss,
+            "freq_ce_loss": freq_ce_loss,
+            "bpm_mae": bpm_mae,
+        }
+
+    @staticmethod
+    def normal_distribution(
+        bpm: torch.Tensor,
+        min_bpm: int,
+        max_bpm: int,
+        std: float,
+    ) -> Dict[str, torch.Tensor]:
+        scaled_bpm = bpm - min_bpm
+        range_size = max_bpm - min_bpm
+        bpm_range = (
+            torch.arange(
+                0,
+                range_size,
+            )
+            .float()
+            .unsqueeze(0)
+        )
+        diff = bpm_range - scaled_bpm
+        exponent = -(diff**2) / (2 * std**2)
+        normal_distribution = torch.exp(exponent) / (
+            torch.sqrt(2 * torch.tensor(math.pi)) * std
+        )
+        normal_distribution = torch.clamp(
+            normal_distribution,
+            min=1e-15,
+        )
+        return {
+            "normal_distribution": normal_distribution,
+            "bpm_range": bpm_range,
+        }
+
+    @staticmethod
+    def kl_divergence_loss(
+        pred: torch.Tensor,
+        target: torch.Tensor,
+    ) -> torch.Tensor:
+        criterion = nn.KLDivLoss(
+            reduction="batchmean",
+            log_target=False,
+        )
+        log_pred = F.log_softmax(
+            pred,
+            dim=-1,
+        )
+        loss = criterion(
+            input=log_pred,
+            target=target,
+        )
+        return loss
+
+    def negative_pearson_loss(
+        self,
+        pred: torch.Tensor,
+        target: torch.Tensor,
+    ) -> torch.Tensor:
+        pred_mean = pred.mean(
+            dim=-1,
+            keepdim=True,
+        )
+        target_mean = target.mean(
+            dim=-1,
+            keepdim=True,
+        )
+
+        pred_centered = pred - pred_mean
+        target_centered = target - target_mean
+
+        pred_std = pred_centered.pow(2).sum(dim=-1).sqrt()
+        target_std = target_centered.pow(2).sum(dim=-1).sqrt()
+
+        covariance = (pred_centered * target_centered).sum(dim=-1)
+
+        pearson_correlation = covariance / (pred_std * target_std)
+
+        loss = 1 - pearson_correlation
+        return loss.mean()
+
+    def pred_frequency_distribution(
+        self,
+        pred: torch.Tensor,
+        absolute_beat: torch.Tensor,
+        num_signals: int,
+    ) -> torch.Tensor:
+        angular_frequencies = (
+            2
+            * torch.pi
+            * torch.arange(
+                0,
+                num_signals,
+                dtype=pred.dtype,
+                device=pred.device,
+            )
+        ) / num_signals
+        angular_frequencies = angular_frequencies.unsqueeze(0).unsqueeze(0)
+        hanning_window = torch.hann_window(
+            num_signals,
+            dtype=pred.dtype,
+            device=pred.device,
+        ).unsqueeze(0)
+        pred = pred * hanning_window
+        absolute_beat = absolute_beat.unsqueeze(-1).to(
+            pred.device,
+            dtype=pred.dtype,
+        )
+        pred_frequency_distribution = (
+            torch.einsum(
+                "B S, B F S -> B F",
+                pred,
+                torch.sin(absolute_beat * angular_frequencies),
+            )
+            ** 2
+            + torch.einsum(
+                "B S, B F S -> B F",
+                pred,
+                torch.cos(absolute_beat * angular_frequencies),
+            )
+            ** 2
+        )
+        return pred_frequency_distribution
+
+    def normalized_pred_frequency_distribution(
+        self,
+        pred: torch.Tensor,
+        frame_rate: torch.Tensor,
+        bpm_range: torch.Tensor,
+    ) -> torch.Tensor:
+        num_signals = pred.size(-1)
+        frequency = frame_rate / num_signals
+        bps_range = bpm_range / 60.0
+        absolute_beat = bps_range / frequency
+        pred_frequency_distribution = self.pred_frequency_distribution(
+            pred=pred,
+            absolute_beat=absolute_beat,
+            num_signals=num_signals,
+        )
+        normalized_pred_frequency_distribution = (
+            1.0 / pred_frequency_distribution.sum()
+        ) * pred_frequency_distribution
+        return normalized_pred_frequency_distribution
+
+    def combined_loss(
+        self,
+        pred: torch.Tensor,
+        bpm: torch.Tensor,
+        min_bpm: int,
+        max_bpm: int,
+        std: float,
+        frame_rate: torch.Tensor,
+    ) -> Dict[str, torch.Tensor]:
+        distribution_outputs = self.normal_distribution(
+            bpm=bpm,
+            min_bpm=min_bpm,
+            max_bpm=max_bpm,
+            std=std,
+        )
+        bpm_distribution = distribution_outputs["normal_distribution"]
+        bpm_range = distribution_outputs["bpm_range"]
+
+        normalized_pred_frequency_distribution = (
+            self.normalized_pred_frequency_distribution(
+                pred=pred,
+                frame_rate=frame_rate,
+                bpm_range=bpm_range,
+            )
+        )
+
+        kl_div_loss = self.kl_divergence_loss(
+            pred=normalized_pred_frequency_distribution,
+            target=bpm_distribution,
+        )
+
+        freq_ce_loss = F.cross_entropy(
+            input=normalized_pred_frequency_distribution,
+            target=bpm,
+        )
+        bpm_mae = F.l1_loss(
+            input=torch.max(
+                normalized_pred_frequency_distribution,
+                dim=-1,
+            )[0],
+            target=bpm,
+        )
+        return {
             "kl_div_loss": kl_div_loss,
             "freq_ce_loss": freq_ce_loss,
             "bpm_mae": bpm_mae,
