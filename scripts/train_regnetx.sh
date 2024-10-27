28e9202a9428fc5201f697ae4a71fc6b94c4b5af
DimensionSTP <ddang8jh@gmail.com>
2024-10-28 01:57:33 +0900

    delete: old scripts

--- scripts/train_regnetx.sh
+++
@@ -1,6 +0,0 @@
-model_list="regnetx_002 regnetx_004 regnetx_006 regnetx_008 regnetx_016 regnetx_032 regnetx_040 regnetx_064 regnetx_080 regnetx_120 regnetx_160"
-
-for model in $model_list
-do
-    HYDRA_FULL_ERROR=1 CUDA_LAUNCH_BLOCKING=1 python main.py mode=train is_tuned=untuned backbone=$model batch_size=64
-done
--- scripts/train_regnety.sh
+++
@@ -1,6 +0,0 @@
-model_list="regnety_002 regnety_004 regnety_006 regnety_008 regnety_016 regnety_032 regnety_040 regnety_064 regnety_080 regnety_120 regnety_160"
-
-for model in $model_list
-do
-    HYDRA_FULL_ERROR=1 CUDA_LAUNCH_BLOCKING=1 python main.py mode=train is_tuned=untuned backbone=$model batch_size=64
-done
