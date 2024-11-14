ca10a593979cd8ab397148681601311917dc1544
DimensionSTP <ddang8jh@gmail.com>
2024-11-14 15:36:28 +0900

    feat: add workers ratio and use all workers options

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,8 @@ is_pretrained=False
 strategy="ddp"
 precision=32
 batch_size=128
+workers_ratio=8
+use_all_workers=False
 epochs="9 10"
 
 for epoch in $epochs
@@ -15,5 +17,7 @@ do
         strategy=$strategy \
         precision=$precision \
         batch_size=$batch_size \
+        workers_ratio=$workers_ratio \
+        use_all_workers=$use_all_workers \
         epoch=$epoch
 done
--- scripts/train.sh
+++ scripts/train.sh
@@ -5,10 +5,14 @@ is_pretrained=False
 strategy="ddp"
 precision=32
 batch_size=128
+workers_ratio=8
+use_all_workers=False
 
 python main.py mode=train \
     is_tuned=$is_tuned \
     is_pretrained=$is_pretrained \
     strategy=$strategy \
     precision=$precision \
-    batch_size=$batch_size
+    batch_size=$batch_size \
+    workers_ratio=$workers_ratio \
+    use_all_workers=$use_all_workers
