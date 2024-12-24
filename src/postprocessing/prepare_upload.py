e2d20fda1973629d25de4167c027cfb3e28cec67
DimensionSTP <ddang8jh@gmail.com>
2024-12-24 18:05:09 +0900

    fix: prepare and upload modules

--- src/postprocessing/prepare_upload.py
+++ src/postprocessing/prepare_upload.py
@@ -5,100 +5,64 @@ dotenv.load_dotenv(
 )
 
 import os
-import re
+import warnings
+
+os.environ["HYDRA_FULL_ERROR"] = "1"
+os.environ["HF_HOME"] = os.environ.get("HF_HOME")
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+warnings.filterwarnings("ignore")
+
 import json
 
 import torch
 
-from safetensors.torch import save_file
-
-from tqdm import tqdm
-
 import hydra
-from omegaconf import DictConfig
+from omegaconf import OmegaConf, DictConfig
 
 
 @hydra.main(
     config_path="../../configs/",
-    config_name="rhythm.yaml",
+    config_name="physformer.yaml",
 )
 def prepare_upload(
     config: DictConfig,
 ) -> None:
-    save_dir = f"{config.connected_dir}/prepare_upload/{config.model_detail}/epoch={config.epoch}"
-    if not os.path.exists(save_dir):
-        os.makedirs(
-            save_dir,
-            exist_ok=True,
+    if config.is_tuned == "tuned":
+        params = json.load(
+            open(
+                config.tuned_hparams_path,
+                "rt",
+                encoding="UTF-8",
+            )
         )
+        config = OmegaConf.merge(
+            config,
+            params,
+        )
+    elif config.is_tuned == "untuned":
+        pass
+    else:
+        raise ValueError(f"Invalid is_tuned argument: {config.is_tuned}")
+
+    save_dir = f"{config.connected_dir}/prepare_upload/{config.model_detail}/{config.upload_tag}/epoch={config.epoch}"
+    os.makedirs(
+        save_dir,
+        exist_ok=True,
+    )
 
     if config.strategy.startswith("deepspeed"):
         checkpoint = torch.load(f"{config.ckpt_path}/model.pt")
     else:
         checkpoint = torch.load(config.ckpt_path)
-    checkpoint_state_dict = checkpoint["state_dict"]
-    model_state_dict = {}
-    for k, v in list(checkpoint_state_dict.items()):
-        if k.startswith("model."):
-            k = re.sub(
-                r"(model\.)+(.*)",
-                r"model.\2",
-                k,
-            )
-            if k.startswith("model.lm_head"):
-                k = k.replace(
-                    "model.",
-                    "",
-                )
-            model_state_dict[k] = v
 
-    if config.precision == 32 or config.precision == "32":
-        safetensors_dtype = torch.float32
-    elif config.precision == 16 or config.precision == "16":
-        safetensors_dtype = torch.float16
-    elif config.precision == "bf16":
-        safetensors_dtype = torch.bfloat16
-    else:
-        raise ValueError(f"Invalid precision type: {config.precision}")
-
-    state_dict = model_state_dict
-    keys = list(state_dict.keys())
-    num_splits = config.num_safetensors
-    split_size = len(keys) // num_splits
-    total_size = sum(
-        param.numel() * param.element_size() for param in state_dict.values()
+    model_save_path = os.path.join(
+        save_dir,
+        "model.ckpt",
     )
-    index_dict = {
-        "metadata": {
-            "total_size": total_size,
-        },
-        "weight_map": {},
-    }
-
-    for i in tqdm(range(num_splits)):
-        start_idx = i * split_size
-        end_idx = (i + 1) * split_size if i < num_splits - 1 else len(keys)
-        safe_tensors_name = f"model-{i+1:05d}-of-{num_splits:05d}.safetensors"
-        part_state_dict = {
-            k: state_dict[k].to(safetensors_dtype) for k in keys[start_idx:end_idx]
-        }
-        part_state_dict_mapping = {
-            k: safe_tensors_name for k in keys[start_idx:end_idx]
-        }
-        index_dict["weight_map"].update(part_state_dict_mapping)
-        save_file(
-            part_state_dict,
-            f"{save_dir}/{safe_tensors_name}",
-            metadata={
-                "format": "pt",
-            },
-        )
-    with open(f"{save_dir}/model.safetensors.index.json", "w") as f:
-        json.dump(
-            index_dict,
-            f,
-            indent=2,
-        )
+    torch.save(
+        checkpoint,
+        model_save_path,
+    )
 
 
 if __name__ == "__main__":
